b8e2634239f8b3b7f5363a7bb4266fce65b33902
root <root@raspberrypi>
2014-08-12 11:44:59 +0400

    Make protocol for nvt-1n

--- regweight.py
+++ regweight.py
@@ -7,19 +7,18 @@ from logger import *
 from db import *
 
 DEVICE='/dev/ttyUSB0'
-BAUDRATE=38400
+BAUDRATE=9600
 ADDR='\x01'
 SELF_PATH=path.dirname(path.realpath(__file__))
 LOG_PATH=path.join(SELF_PATH,'regweight')
 DUMP_FILE=path.join(SELF_PATH,'regweight.dump')
-TABLE_WEIGHT=107.5
-FILTER = 50
+TABLE_WEIGHT=0
+FILTER = 200
 
-class Tenso:
-    __MINUS=0x80
-    __SEDATION=0x10
+class NVT1N:
+    __SEDATION=0x02
     __OVERLOAD=0x08
-    __SEPARATOR=0x07
+    __NULL=0x01
 
     def __init__(self,device=DEVICE,baudrate=BAUDRATE,addr=ADDR):
         self.__ser = serial.Serial()
@@ -30,7 +29,7 @@ class Tenso:
         self.__addr=addr
         self.log=Logger('regweight')
         self.debug=False
-        self.__pattern_kadr=re.compile('\xff+(?P<data>.*?)(?P<crc>\xff\xfe|[^\xff])\xff{2,}')
+        self.__pattern_kadr=re.compile('\x02(?P<data>.{8})\x6b\x67\x0d')
         self.__d=DBreg(SELF_PATH)
 
 
@@ -53,18 +52,10 @@ class Tenso:
         chk=self.CRC(data)
         return chk=='\x00'
 
-    def query(self,data):
+    def query(self):
         res=None
-        d=self.__addr+data+'\x00'
-        crc=self.CRC(d)
-        if crc=='\xff': crc=crc+'\xfe'
-        d='\xff'+d[:-1].replace('\xff','\xff\xfe')+crc+'\xff\xff'
-
         try:
             if self.__ser.isOpen():
-                self.__ser.flushInput()
-                self.__ser.write(d)
-                self.__ser.flush()
                 trys=0
                 outlen=0
                 while (outlen==0)and(trys<(self.__ser.timeout*10)):
@@ -72,14 +63,12 @@ class Tenso:
                     outlen=self.__ser.inWaiting()
                     time.sleep(0.1)
 
-                if 0<outlen<255:
+                if outlen>0:
                     ans=self.__ser.read(outlen)
 
                     out_obj=self.__pattern_kadr.search(ans)
-                    outdata=out_obj.group('data').replace('\xff\xfe','\xff')
-                    outcrc=out_obj.group('crc').replace('\xff\xfe','\xff')
-                    if self.checkCRC(outdata+outcrc):
-                        res=outdata[2:] if (len(outdata)>1) else None
+                    outdata=out_obj.group('data')
+                    res=outdata if (len(outdata)>1) else None
             else:
                 self.__ser.open()
         except Exception as e:
@@ -90,6 +79,17 @@ class Tenso:
             #self.__ser.close()
             pass
         return res
+    
+    def testquery(self):
+	if self.__ser.isOpen():
+	    outlen=self.__ser.inWaiting()
+	    if outlen>0:
+		ans=self.__ser.read(outlen)
+		time.sleep(0.1)
+		self.log.d('hex query ANSWER: %s' % " ".join(["%02x" % ord(x) for x in ans]))
+		self.log.d('ansi query answer: %s' % " ".join(["%s" % x for x in ans]))
+	else:
+	    self.__ser.open()
 
     def decodeBCD(self,bcd):
         res=["%02x" % ord(x) for x in bcd]
@@ -97,20 +97,15 @@ class Tenso:
         return list("".join(res))
 
 
+
     def getBRUTTO(self):
         weight=None
-        data='\xc3'
-        res=self.query(data)
+        res=self.query()
         if res is None: return None,False
-
-        weight_digs=self.decodeBCD(res[:-1])
+        
         con=ord(res[-1:])
         if (con & self.__OVERLOAD) != self.__OVERLOAD:
-            sep = con & self.__SEPARATOR
-            weight_digs.insert(6-sep,'.')
-            if (con & self.__MINUS)==self.__MINUS:
-                weight_digs.insert(0,'-')
-            weight=float("".join(weight_digs))
+	    weight=float(res[:-1].strip())
         return weight,((con & self.__SEDATION)==self.__SEDATION)
 
     def regWeight(self,weight):
@@ -122,13 +117,13 @@ class Tenso:
             return False
 
 if __name__ == "__main__":
-    tenso=Tenso(DEVICE,BAUDRATE,ADDR)
-    tenso.debug=True
+    nvt=NVT1N(DEVICE,BAUDRATE,ADDR)
+    nvt.debug=True
     old_weight=0
     cnt=0
     must_dump=False
     starting=True
-    tenso.log.c("STARTING...")
+    nvt.log.c("STARTING...")
 
     if path.isfile(DUMP_FILE):
         with open(DUMP_FILE,"rb") as dump:
@@ -136,36 +131,38 @@ if __name__ == "__main__":
 
     while True:
         try:
-            weight,sedation=tenso.getBRUTTO()
-            if tenso.debug:
-                tenso.log.d(__name__+": weight = %1.1f (%i)" % (weight,sedation))
+	    #nvt.testquery()
+	    #nvt.log.d(nvt.query())
+            weight,sedation=nvt.getBRUTTO()
+            if nvt.debug:
+                nvt.log.d(__name__+": weight = %1.1f (%i)" % (weight,sedation))
             if weight is None: continue
 
-            if (sedation) and (starting) and (abs(weight-TABLE_WEIGHT)<1):
-                continue
+            #if (sedation) and (starting) and (abs(weight-TABLE_WEIGHT)<1):
+            #    continue
 
-            if (abs(weight)<FILTER):
-                must_dump=not(old_weight==0)
-                old_weight=0
-                starting=False
-            elif (old_weight==0) and (not starting):
-                if (sedation):
-                    cnt+=1
-                    if (cnt>2) and tenso.regWeight(weight):
-                        old_weight=weight
-                        must_dump=True
-                        cnt=0
-                else:
-                    cnt=0
-            if must_dump:
-                with open(DUMP_FILE,"wb") as dump:
-                    pickle.dump(old_weight,dump)
-                must_dump=False
+            #if (abs(weight)<FILTER):
+            #    must_dump=not(old_weight==0)
+            #    old_weight=0
+            #    starting=False
+            #elif (old_weight==0) and (not starting):
+            #    if (sedation):
+            #        cnt+=1
+            #        if (cnt>2) and tenso.regWeight(weight):
+            #            old_weight=weight
+            #            must_dump=True
+            #            cnt=0
+            #    else:
+            #        cnt=0
+            #if must_dump:
+            #    with open(DUMP_FILE,"wb") as dump:
+            #        pickle.dump(old_weight,dump)
+            #    must_dump=False
 
         except Exception as e:
-            tenso.log.e(__name__+' ERROR: %s' % e)
+            nvt.log.e(__name__+' ERROR: %s' % e)
             continue
         finally:
-            time.sleep(0.5)
+            time.sleep(1)
 
 
